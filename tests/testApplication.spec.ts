7d06d0ae106e462946190f2059f4d7477d79e406
Arun kumar <arunkumarsra01@gmail.com>
2026-03-07 15:26:07 +0530

    removed incorrect groups

--- tests/testApplication.spec.ts
+++ tests/testApplication.spec.ts
@@ -10,19 +10,3 @@ test('testing dependency injection-part 2', async ({ loginpage }) => {
     await loginpage.registerForSubscription('adminuser@hooli.com')
     await expect(loginpage.subscriptionMessage).toHaveText('Thank you for signing up! A verification email has been sent. We appreciate your interest.')
 });
-
-
-test('Alert hadling', async ({ page }) => {
-    await page.goto('https://example.com');
-    page.on('dialog', dialog => dialog.accept());
-    await page.getByRole('button', { name: 'ClickonAlert' }).click();
-})
-
-test('Handling tabs', async ({ browser }) => {
-    const context = await browser.newContext();
-    const page = await context.newPage()
-    await page.goto('https://example.com');
-    const tab = context.waitForEvent('page');
-    const newPage = await tab;
-    await expect(newPage).toHaveTitle(/demo/)
-})
